e0aa9b73a606f490e48d5b409a8a1dfa7ec52dde
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2024-12-22 11:31:23 +0100

    Update unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,6 +9,13 @@ describe('CacheZap', () => {
     jest.useRealTimers();
   });
 
+  test('raises an error if the duration is less than 1', () => {
+    const cache = new CacheZap<string, number>();
+    expect(() => {
+      cache.set('key1', 100, 0);
+    }).toThrow('Duration must be a positive number.');
+  });
+
   test('stores and retrieve a value', () => {
     const cache = new CacheZap<string, number>();
     cache.set('key1', 100, 1000);
@@ -47,5 +54,13 @@ describe('CacheZap', () => {
     cache.set('key2', 200, 1000);
     cache.clear();
     expect(cache.size()).toBe(0);
+  });
+
+  test('check if a key exists and it is not expired', () => {
+    const cache = new CacheZap<string, number>();
+    cache.set('key1', 100, 1000);
+    expect(cache.has('key1')).toBeTruthy();
+    jest.advanceTimersByTime(1001);
+    expect(cache.has('key1')).toBeFalsy();
   });
 });
